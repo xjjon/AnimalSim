ff500ecae546f0d556bf170630437489cb841193
xjjon <oninuva@gmail.com>
2025-08-04 16:06:58 -0600

    Add working wander behavior

--- Assets/Scripts/Core/AI/Actions/WanderAction.cs
+++ Assets/Scripts/Core/AI/Actions/WanderAction.cs
@@ -1,12 +1,34 @@
 
 using Core.Animals.Movement;
 using NodeCanvas.Framework;
+using UnityEngine;
+using UnityEngine.AI;
 
 namespace Core.AI.Actions
 {
     public class WanderAction : ActionTask<MovementController> {
-        protected override void OnExecute() {
+        
+        public float WanderRadius = 10f;
 
+        private NavMeshAgent _navMeshAgent;
+
+        protected override string OnInit()
+        {
+            _navMeshAgent = agent.GetComponent<NavMeshAgent>();
+            return base.OnInit();
+        }
+
+    
+        protected override void OnExecute()
+        {
+            Vector3 randomDirection = Random.insideUnitSphere * WanderRadius;
+            randomDirection += agent.transform.position;
+
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomDirection, out hit, WanderRadius, NavMesh.AllAreas);
+
+            _navMeshAgent.SetDestination(hit.position);
+            EndAction(true);
         }
     }
 }
